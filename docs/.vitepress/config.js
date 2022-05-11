0c5ecca4b55e991aaadf544fa8edf0abce67b62b
전지원(Jiwon Jeon)/Core플랫폼개발팀/11ST <jiwon_jeon@sk.com>
2022-05-11 13:18:45 +0900

    feat: Initial Commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,64 @@
+export default {
+    title: 'Diary',
+    description: 'Feelings',
+    base: '/diary/',
+    lang: 'ko-KR',
+    head: [
+        ['meta', {
+            name: 'theme-color',
+            content: '#3eaf7c'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: 'black'
+        }],
+        ['meta', {
+            name: 'og:image',
+            //   content: 'https://h4dvprmht2.execute-api.ap-northeast-2.amazonaws.com/automated-og-image?text=Today%20I%20Learned'
+        }],
+        [
+            'script',
+            {
+                async: true,
+                src: 'https://www.googletagmanager.com/gtag/js?id=G-3DJWRRKTMB',
+            },
+        ],
+        [
+            'script',
+            {},
+            [
+                "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-99GK4TE88W');",
+            ],
+        ],
+    ],
+    themeConfig: {
+        repo: '',
+        editLinks: false,
+        docsDir: '',
+        editLinkText: '',
+        lastUpdated: false,
+        nav: [{
+                text: 'Github',
+                link: 'https://github.com/kworkbee',
+            },
+            {
+                text: 'Blog',
+                link: 'https://about.g1tommy.me/'
+            },
+            {
+                text: 'Profile',
+                link: 'https://about.g1tommy.me/resume'
+            }
+        ],
+        sidebar: [{
+            title: '220511',
+            children: [
+                '220511'
+            ]
+        }]
+    }
+}
